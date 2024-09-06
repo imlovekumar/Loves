f1af7d96cff2c3af955911598107a92c1a8c44c2
imlovekumar <64594649+imlovekumar@users.noreply.github.com>
2024-09-06 17:38:45 +0530

    Update book.mjs

--- book.mjs
+++ book.mjs
@@ -4,7 +4,7 @@ const params =
     "ManualCaptcha" : "N",      // For Manual Entry  (Y/N)
     "PremiumCaptcha": "N",      //For 100 Sucess Rate  (Y/N)
     "firstday" : "N",             //only In case 120 Days Before (Y/N)
-    "UPI": "imloveairtel@ybl",
+    "UPI": "@ybl",
     "class": "3A",
     "quota": "GN",
     "train_number": "12163",
@@ -12,8 +12,8 @@ const params =
     "to": "RU",
     "journey_date": "20240909",
     "mobile_number": "7987094362",
-    "userID": "PPK01",
-    "password": "9024",
+    "userID": "",
+    "password": "",
     "passengers": 
     [
         {
