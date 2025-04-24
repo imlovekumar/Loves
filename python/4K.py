6a7b0b89d0cf6a7da6126b83ea407d50c2bb6b27
imlovekumar <64594649+imlovekumar@users.noreply.github.com>
2025-04-24 13:16:41 +0530

    Update 4K.py

--- python/4K.py
+++ python/4K.py
@@ -84,8 +84,7 @@ def find_channel_by_number(token, user_input_number):
             return {
                 "number": channel.get("number"),
                 "name": channel.get("name"),
-                "cmd": channel.get("cmd"),
-                "logo": channel.get("logo")
+                "cmd": channel.get("cmd")
             }
     return None
 
@@ -108,7 +107,7 @@ def build_m3u_playlist(channels):
     lines = [""]
     for channel in channels:
         lines.append(
-            f'#EXTINF:-1 tvg-id="{channel["number"]}" tvg-logo="{channel["logo"]}" tvg-name="{channel["name"]}" group-title="{channel["group_title"]}",{channel["name"]}'
+            f'#EXTINF:-1 tvg-id="{channel["number"]}" tvg-name="{channel["name"]}" group-title="{channel["group_title"]}",{channel["name"]}'
         )
         lines.append(channel["url"])
     return "\n".join(lines)
