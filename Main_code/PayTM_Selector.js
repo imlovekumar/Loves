7120c2bc508cdef0c78683efeab8a1654d4c8a29
imlovekumar <64594649+imlovekumar@users.noreply.github.com>
2025-06-14 12:48:04 +0530

    Update PayTM_Selector.js

--- Main_code/PayTM_Selector.js
+++ Main_code/PayTM_Selector.js
@@ -3,34 +3,40 @@
     {
         return new Promise(res => setTimeout(res, ms));
     }
-    function simulateClick(element) {
-        if (element && typeof element.dispatchEvent === "function") {
-            if (typeof element.focus === "function") 
-            {
-                try {  element.focus();    }
-                catch (err) { console.warn("Could not focus element before click:", element, err.message);    }
-            }
-            if (element.disabled) {   console.warn("Attempted to click on a disabled element:", element); return; }
-            const mouseDown = new MouseEvent("mousedown", { bubbles: true, cancelable: true, view: window, button: 0 });
-            const mouseUp = new MouseEvent("mouseup", { bubbles: true, cancelable: true, view: window, button: 0 });
-            const clickEvent = new MouseEvent("click", { bubbles: true, cancelable: true, view: window, button: 0 });
-            element.dispatchEvent(mouseDown);
-            element.dispatchEvent(mouseUp);
-            element.dispatchEvent(clickEvent);
-        } else { console.warn("Invalid element for simulateClick:", element);    }
+    // === Utility: Simulate fast & stealthy click ===
+    function simulateClick(el) {
+        if (!el) return;
+        const delay = ms => new Promise(res => setTimeout(res, ms));
+        const fire = type => {
+            el.dispatchEvent(new MouseEvent(type, {
+                bubbles: true,
+                cancelable: true,
+                clientX: el.getBoundingClientRect().left + 1,
+                clientY: el.getBoundingClientRect().top + 1
+            }));
+        };
+        return (async () => {
+            fire('mouseover');
+            await delay(10 + Math.random() * 10);
+            fire('mousedown');
+            await delay(10 + Math.random() * 15);
+            fire('mouseup');
+            await delay(5 + Math.random() * 10);
+            fire('click');
+        })();
     }
 
     let paymentGroup = "BHIM/ UPI/ USSD";
     let paymentOption = "Pay using BHIM (Powered by PAYTM ) also accepts UPI";
     let found = false;
-    console.log("Searching",paymentOption);
+    //console.log("Searching",paymentOption);
     let interval = setInterval(() => {
         if (document.getElementsByClassName("bank-type").length > 1) {
             clearInterval(interval);
             let groups = document.getElementById("pay-type").getElementsByTagName("div");
             for (let i = 0; i < groups.length; i++) {
                 if (groups[i].innerText.includes(paymentGroup)) {
-                    console.log(paymentGroup,"Found !");
+                    //console.log(paymentGroup,"Found !");
                     simulateClick(groups[i]);
                     console.log(paymentGroup,"Selected !");
                     setTimeout(() => {
@@ -38,7 +44,7 @@
                         for (let j = 0; j < methods.length; j++) {
                             let label = methods[j].getElementsByTagName("span")[0]?.innerText || "";
                             if (methods[j].getBoundingClientRect().top !== 0 && label.toUpperCase().includes(paymentOption.toUpperCase())) {
-                                console.log(paymentOption,"Found !");
+                                //console.log(paymentOption,"Found !");
                                 simulateClick(methods[j]);
                                 console.log(paymentOption,"Selected !");
                                 found = true;
@@ -49,7 +55,7 @@
                                 setTimeout(() => {
                                     console.log("Auto Pay & Book Clicked !");
                                     simulateClick(document.getElementsByClassName("btn-primary")[0]);
-                                }, 500);
+                                }, 1000);
                                 break;
                             }
                         }
