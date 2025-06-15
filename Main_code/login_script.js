2a53acf156b0beb511a4ffffc64db15fef901922
imlovekumar <64594649+imlovekumar@users.noreply.github.com>
2025-06-15 16:04:11 +0530

    Update login_script.js

--- Main_code/login_script.js
+++ Main_code/login_script.js
@@ -92,64 +92,6 @@
         })();
     }
 
-    async function solveCaptcha(e = 0) 
-    {
-        if (e >= 100) return;
-        const t = document.querySelector(".captcha-img");
-        if (!t || !t.src || t.src.length < 23) return setTimeout(() => solveCaptcha(e + 1), 1e3);
-        console.log("Captcha Found !");
-        const n = JSON.stringify({  userid: "__userid__",
-                                    apikey: "__apikey__",
-                                    data: t.src.slice(22)
-                                });
-        try 
-        {
-            const r = await fetch("https://api.apitruecaptcha.org/one/gettext", 
-            {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: n
-            });
-            if (!r.ok) return console.error("Captcha API error:", r.status, r.statusText);
-            console.log("Captcha Solved !");
-            const {result: a = ""} = await r.json();
-            let s = "";
-            for (const c of a.replace(/\s/g, "").replace(/[)\]]/g, "J")) "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=@".includes(c) && (s += c);
-            const i = document.querySelector("#captcha");
-            if (i && s) 
-            {   
-                console.log("Captcha Typings !");
-                await simulateTyping(i, s, config.typingOptions);
-                setTimeout(() => 
-                    {
-                        const submit = document.querySelector("#review > div.col-lg-9.col-md-9.col-sm-12.remove-padding > form > div.form-group.col-xs-12.hidden-xs > div > button.btnDefault.train_Search");
-                        if(submit){
-                            console.log("✔ Submit Button Found !");
-                            simulateClick(submit);
-                            console.log("✍️ Auto Submit !");                            
-                        }
-                        else {
-                            const Log_Btn = document.querySelector("#login_header_disable > div > div > div.ng-tns-c19-13.ui-dialog-content.ui-widget-content > div.irmodal.ng-tns-c19-13 > div > div.login-bg.pull-left > div > div.modal-body > form > span > button");
-                            if(!Log_Btn) {
-                                console.log("❌ Login Button Not Found !");
-                                console.log("✍️ Login Manually ! ");
-                            }else {
-                            console.log("✔ Login Button Found !");
-                            simulateClick(Log_Btn);
-                            console.log("✍️ Auto Login !");
-                            }
-                        }
-                    }, 500)
-            }
-            if (!a) 
-            {
-                const reloadcap = document.querySelector(".glyphicon.glyphicon-repeat")?.parentElement;
-                simulateClick(reloadcap);
-                setTimeout(() => solveCaptcha(e + 1), 500)
-            }
-        } catch (o) { console.error("TrueCaptcha API call failed", o)}
-    }
-
     // === Main Automation Flow (Up to password only) ===
     (async () => {
         try {
@@ -173,7 +115,6 @@
 
             // 4. Log: now solve CAPTCHA manually
             console.log('✅ Username & password filled.\nNow click captcha solve button.');
-            await solveCaptcha();
 
         } catch (err) {
             console.error('❌ Automation error:', err);
