346fcb5bb4296d3da1171d991c752dd5e1c19ad2
imlovekumar <64594649+imlovekumar@users.noreply.github.com>
2025-06-26 22:15:59 +0530

    Update fill_pass.js

--- Main_code/fill_pass.js
+++ Main_code/fill_pass.js
@@ -7,21 +7,15 @@
     function submitPassengerDetailsForm(e) 
     {
         console.log("✓ Passenger Filling Completed");
-
-        if (user_data.other_preferences.psgManual) 
-        {
+        if (user_data.other_preferences.psgManual) {
             console.log("PLz Manually submit !");
             btn = e.querySelector("#psgn-form > form div > button.train_Search.btnDefault[type='submit']")
             scrollToView(btn);
-        } 
-        else 
-        {
+        } else {
             var keyCounter = new Date().getTime();
-            var t = setInterval(function() 
-            {
+            var t = setInterval(function() {
                 var o = new Date().getTime();
-                if (o - keyCounter > 300) 
-                {
+                if (o - keyCounter > 300) {
                     clearInterval(t);
                     console.log("✓ Auto Submit");
                     btn = e.querySelector("#psgn-form > form div > button.train_Search.btnDefault[type='submit']")
@@ -37,9 +31,7 @@
         const rect = element.getBoundingClientRect();
         const x = rect.left + Math.random() * rect.width;
         const y = rect.top + Math.random() * rect.height;
-
         const events = ["mouseover", "mouseenter", "mousemove", "mousedown", "mouseup", "click"];
-
         for (const type of events) {
             const event = new MouseEvent(type, {
                 bubbles: true,
@@ -51,29 +43,23 @@
             element.dispatchEvent(event);
             await new Promise(res => setTimeout(res, Math.random() * 60 + 40));
         }
-    
     }
+    
     async function typeTextHumanLike(e, text) {
         if (!e || typeof text !== "string") return;
-
         await simulateMouseInteraction(e);  // Hover + Click
         e.focus();
         e.value = "";
         e.dispatchEvent(new Event("input", { bubbles: true, cancelable: true }));
-
         for (const char of text) {
             e.dispatchEvent(new KeyboardEvent("keydown", { key: char, bubbles: true }));
             e.value += char;
             e.dispatchEvent(new InputEvent("input", { bubbles: true, cancelable: true, data: char, inputType: "insertText" }));
             e.dispatchEvent(new KeyboardEvent("keyup", { key: char, bubbles: true }));
-
             await new Promise(res => setTimeout(res, Math.random() * 100 + 50));
         }
-
         e.dispatchEvent(new Event("change", { bubbles: true }));
     }
-
-    function addDelay(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
     // === Utility: Simulate fast & stealthy click ===
     function simulateClick(el) {
@@ -110,77 +96,62 @@
     {
         const e = document.querySelector("app-passenger-input");
         if (!e) return alert("Not on the right page.");
-        for (let i = 1; i < user_data.passenger_details.length; i++) 
-        {
+        for (let i = 1; i < user_data.passenger_details.length; i++) {
             simulateClick(document.getElementsByClassName("prenext")[0]);
             await humanDelay(100, 300);
             console.log("All",user_data.passenger_details.length,"Passenger Box Open");
         }
-
         const o = [...e.querySelectorAll("app-passenger")];
-
-        for (let i = 0; i < user_data.passenger_details.length; i++)
-        {
+        for (let i = 0; i < user_data.passenger_details.length; i++){
             let el = o[i];
+            
             const pnameInput = el.querySelector("p-autocomplete input");
-            if (pnameInput) 
-            {await typeTextHumanLike(pnameInput, user_data.passenger_details[i].name);}
+            if (pnameInput) {
+                await typeTextHumanLike(pnameInput, user_data.passenger_details[i].name);
+            }
 
             const pageInput = el.querySelector("input[formcontrolname='passengerAge']");
-            if(pageInput)
-            {await typeTextHumanLike(pageInput, user_data.passenger_details[i].age);}
+            if(pageInput){
+                await typeTextHumanLike(pageInput, user_data.passenger_details[i].age);
+            }
 
             el.querySelector("select[formcontrolname='passengerGender']").value = user_data.passenger_details[i].gender;
             el.querySelector("select[formcontrolname='passengerGender']").dispatchEvent(new Event("change"));
-
             el.querySelector("select[formcontrolname='passengerBerthChoice']").value = user_data.passenger_details[i].berth;
             el.querySelector("select[formcontrolname='passengerBerthChoice']").dispatchEvent(new Event("change"));
-
             let food = el.querySelector("select[formcontrolname='passengerFoodChoice']");
-            if (food) 
-            {
+            if (food) {
                 food.value = user_data.passenger_details[i].food;
                 food.dispatchEvent(new Event("change"));
             }
-
-            try 
-            {
+            try {
                 let chk = el.querySelector("input[type='checkbox'][formcontrolname='childBerthFlag']");
                 if (chk && p.passengerchildberth) chk.click();
             }
             catch (err) { console.error("Child berth error", err); }
         };
         console.log("👬🏾 All Passenger Detail Filled !");
-
-
-        if (user_data.other_preferences.mobileNumber) 
-        {
+        if (user_data.other_preferences.mobileNumber) {
             let m = e.querySelector("input#mobileNumber");
             if (m) { 
                 await typeTextHumanLike(m, user_data.other_preferences.mobileNumber);
                 console.log("📞 Mobile Number Filled !");
             }
         }
-
-
         let upg = e.querySelector("input#autoUpgradation");
-        if (upg && user_data.other_preferences.autoUpgradation !== upg.checked) 
-        {
+        if (upg && user_data.other_preferences.autoUpgradation !== upg.checked) {
             scrollToView(upg);
             await humanDelay();
             simulateClick(upg);
             console.log("✔ Auto Upgradation Checked !");
         }
-
         let conf = e.querySelector("input#confirmberths");
-        if (conf && user_data.other_preferences.confirmberths !== conf.checked)
-        {
+        if (conf && user_data.other_preferences.confirmberths !== conf.checked){
             scrollToView(conf);
             await humanDelay();
             simulateClick(conf);
             console.log("✔ Only Confirmed Seat Checked !");
         }
-
         const insVal = user_data.travel_preferences.travelInsuranceOpted === "yes" ? "true" : 'false';
         const ins = [...e.querySelectorAll("p-radiobutton[formcontrolname='travelInsuranceOpted'] input")].find(q => q.value === insVal);
         if (ins) { 
@@ -189,22 +160,16 @@
             simulateClick(ins);
             console.log("📝 Travel Insurance YES !");            
         }
-
-
         let coach = e.querySelector("input[formcontrolname='coachId']");
         if (coach && user_data.travel_preferences.prefcoach) coach.value = user_data.travel_preferences.prefcoach;
-
         const method = user_data.other_preferences.paymentmethod.includes("UPI") ? '2' : '1';
         const payOptions = [...e.querySelectorAll("p-radiobutton[name='paymentType'] input")].find(q => q.value === method);
-        if (payOptions) 
-        {
+        if (payOptions) {
             scrollToView(payOptions);
             await humanDelay();
             simulateClick(payOptions);
             console.log("पे UPI Selected");            
         }
-        
-
         submitPassengerDetailsForm(e);
     } catch (e) { alert("Script error: " + e.message);}
 })();
