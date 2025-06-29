f0dd42e3c4a53c4e1da18f4f36902befa411b468
imlovekumar <64594649+imlovekumar@users.noreply.github.com>
2025-06-29 12:24:30 +0530

    Update login_script.js

--- Main_code/login_script.js
+++ Main_code/login_script.js
@@ -146,15 +146,9 @@
             const loginBtn = await waitForElement(loginButton, elementWait);
             await simulateClick(loginBtn);
 
-            const logads = document.querySelectorAll('img[src*="contents.irctc.co.in"]');
-            if (logads) {
-                logads.remove();
-            }
-            const jal = document.querySelector("#google_ads_iframe_\\/37179215\\/GPT_IRCTC_LOGIN_CAPTCHA_300X250_0");
-            if (jal) {
-                jal.remove();
-            }
-            
+            document.querySelectorAll('img[src*="contents.irctc.co.in"]').forEach(el => el.remove());
+            document.querySelector("#google_ads_iframe_\\/37179215\\/GPT_IRCTC_LOGIN_CAPTCHA_300X250_0").forEach(el => el.remove());
+                        
             // 2. Type username
             const userEl = await waitForElement(username, elementWait);
             await simulateTyping(userEl, unameVal, config.typingOptions);
